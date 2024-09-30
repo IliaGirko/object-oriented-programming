60ff4c179b2747b036d263eb2d421448a35b94da
Илья Гирько <Gggggirr@gmail.com>
2024-09-30 11:46:03 +0300

    ДЗ 14.1 выполнено

--- src/category.py
+++ src/category.py
@@ -14,19 +14,3 @@ class Category:
         self.name = name
         Category.category_count += 1
         Category.product_count += len(products)
-
-
-# ct1 = Category("телефон", "смартфон", ["apple","samsung"])
-# ct2 = Category("телевизор", "ЖК", ["sony"])
-#
-# print(ct1.name)
-# print(ct1.description)
-# print(ct1.products)
-# print(ct1.count_category)
-# print(ct1.count_product)
-# print(ct2.count_category)
-# print(ct2.count_product)
-#
-#
-# print(Category.count_category)
-# print(Category.count_product)
--- src/product.py
+++ src/product.py
@@ -9,11 +9,3 @@ class Product:
         self.price = price
         self.quantity = quantity
         self.name = name
-
-
-pr1 = Product("телефон", "смартфон", 12000.0, 1)
-
-print(pr1.name)
-print(pr1.description)
-print(pr1.price)
-print(pr1.quantity)
